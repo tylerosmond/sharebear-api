79a2c85a00ce6ddcf70372008938dc21e697d4b5
Tyler Osmond <140542331+tylerosmond@users.noreply.github.com>
2024-10-24 12:53:44 -0500

    refactored and setup transaction functionality

--- sharebearapi/models/transaction.py
+++ sharebearapi/models/transaction.py
@@ -4,7 +4,7 @@ from .product import Product
 
 
 class Transaction(models.Model):
-    owner = models.ForeignKey(
+    product_owner = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="transactions_as_owner"
     )
     recipient = models.ForeignKey(
@@ -13,7 +13,8 @@ class Transaction(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name="transactions"
     )
-    exchange_date = models.DateTimeField(auto_now_add=True)
+    request_date = models.DateTimeField(auto_now_add=True)
+    exchange_date = models.DateTimeField(null=True, blank=True)
     status = models.CharField(
         max_length=20,
         default="pending",
@@ -25,7 +26,7 @@ class Transaction(models.Model):
     )
 
     class Meta:
-        unique_together = ("owner", "recipient", "product")
+        unique_together = ("product_owner", "recipient", "product")
 
     def __str__(self):
         return f"{self.product.name} exchanged from {self.owner.username} to {self.recipient.username} on {self.exchange_date}"
--- sharebearapi/serializers/transaction_serializer.py
+++ sharebearapi/serializers/transaction_serializer.py
@@ -1,11 +1,31 @@
+# from rest_framework import serializers
+# from sharebearapi.models.transaction import Transaction
+# from sharebearapi.serializers.product_serializer import ProductSerializer
+
+
+# class TransactionSerializer(serializers.ModelSerializer):
+#     product = ProductSerializer()
+
+#     class Meta:
+#         model = Transaction
+#         fields = ["id", "owner", "recipient", "product", "status", "exchange_date"]
 from rest_framework import serializers
 from sharebearapi.models.transaction import Transaction
-from sharebearapi.serializers.product_serializer import ProductSerializer
+from sharebearapi.models.product import Product
 
 
 class TransactionSerializer(serializers.ModelSerializer):
-    product = ProductSerializer()
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
 
     class Meta:
         model = Transaction
-        fields = ["id", "owner", "recipient", "product", "status", "exchange_date"]
+        fields = [
+            "id",
+            "product_owner",
+            "recipient",
+            "product",
+            "status",
+            "request_date",
+            "exchange_date",
+        ]
+        read_only_fields = ["exchange_date"]
